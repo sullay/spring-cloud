ca2488bc29c87be32127f2db5bb42fd4da6acccd
i-sullay <i-sullay@wicresoft.com>
2017-07-20 09:50:37 +0800

    new

---
+++ serice-feign/src/main/java/com/forezp/service/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.forezp.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
