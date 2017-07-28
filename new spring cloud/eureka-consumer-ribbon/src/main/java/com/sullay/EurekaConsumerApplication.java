37587e2e7f3bbdecfebb8aa458f5db145ec10b76
i-sullay <i-sullay@wicresoft.com>
2017-07-28 10:40:46 +0800

    服务消费（Ribbon)

---
+++ new spring cloud/eureka-consumer-ribbon/src/main/java/com/sullay/EurekaConsumerApplication.java
@@ -0,0 +1,23 @@
+package com.sullay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaConsumerApplication {
+	//初始化RestTemplate，用来真正发起REST请求
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumerApplication.class, args);
+	}
+}
